a1edefa74ccfeea7aa498374b2deb3550af4c061
Edwin Rojas <rojaedwin@gmail.com>
2020-03-29 23:50:30 -0400

    Cambios en DB

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports ={
 
     //exportar objeto configuracion de BD
     database: {
-        host:'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_links'
+        host:'us-cdbr-iron-east-01.cleardb.net',
+        user: 'b84edc25641db3',
+        password: '36094175',
+        database: 'heroku_55e1d1d5ed983d2'
     }
 
 };
