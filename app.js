1981f283d650e2fddf14f58503f67a9bf641a4f1
rojasedwin <rojaedwin@gmail.com>
2022-07-04 11:40:33 -0400

    nuevo sia

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) =>{
     res.send('Hola mundo')
 })
 
-app.listen(5000, ()=>{
-    console.log('Server running in port: ', 5000)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log('Server running in port: ', PORT)
 });
