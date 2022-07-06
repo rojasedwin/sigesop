3368c1d6e2a952b213d7824e9cee980a1dc8e7a0
rojasedwin <rojaedwin@gmail.com>
2022-07-06 10:43:41 -0400

    Plantillas

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+// 1 - Invocamos a Express
+const express = require('express');
+const app = express();
 
-app.set('view engine', 'ejs')
+//2 - Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());//además le decimos a express que vamos a usar json
 
+//3- Invocamos a dotenv
+const dotenv = require('dotenv');
+dotenv.config({ path: './env/.env'});
 
-// import the router
-app.use('/', require('./routes/router'))
-app.use(express.static(path.join(__dirname, '/public')))
+//4 -seteamos el directorio de assets
+app.use('/resources',express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, ()=>{
-    console.log('Server running in port: ', PORT)
+app.listen(3000, (req, res)=>{
+    console.log('SERVER RUNNING IN http://localhost:3000');
 });
