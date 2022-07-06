1130a7cdc5ef4490df56a733da69c73ca353463a
rojasedwin <rojaedwin@gmail.com>
2022-07-05 23:39:12 -0400

    Plantillas

---
+++ database/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql')
+// mysql://bf7ce2d52f141a:4a1944fd@us-cdbr-east-05.cleardb.net/heroku_a1f7b4e8dda28ba?reconnect=true
+/*const conexion = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE,
+})*/
+
+const conexion = mysql.createConnection({
+    host : 'us-cdbr-east-05.cleardb.net',
+    user : 'bf7ce2d52f141a',
+    password : '4a1944fd',
+    database : 'heroku_a1f7b4e8dda28ba'
+})
+
+conexion.connect ((error) => {
+    if(error) {
+        console.error('The connection error is:' + error)
+        return
+    }
+    console.log('Connected to the database MySQL!')
+})
+
+module.exports = conexion
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+
+//Invoke the database connection
+const conexion = require('../database/db')
+
 router.get('/', (req, res) => {
-   res.send('Hola soy mundo');
+   //res.send('Hola soy mundo');
+   // conexion.query('SELECT * FROM users', (error, results) =>{
+   //    if(error){
+   //       throw error;
+   //    }else{
+   //       res.send(results);
+   //    }
+   // })
+   res.render('index')
 })
 
 
