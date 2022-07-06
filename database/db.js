8fedeeb588c5b84e0f7d654d81e42f2558ca1d5c
rojasedwin <rojaedwin@gmail.com>
2022-07-06 14:52:28 -0400

    Plantillas

--- database/db.js
+++ database/db.js
@@ -1,20 +1,38 @@
 //2 - Invocamos a MySQL y realizamos la conexion
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    //Con variables de entorno
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE
-});
+// const connection = mysql.createConnection({
+//     //Con variables de entorno
+//     host     : process.env.DB_HOST,
+//     user     : process.env.DB_USER,
+//     password : process.env.DB_PASS,
+//     database : process.env.DB_DATABASE
+// });
 
 //error
-connection.connect((error)=>{
-    if (error) {
-      console.error('El error de conexión es: ' + error);
-      return;
-    }
-    console.log('¡Conectado a la Base de Datos!');
+// connection.connect((error)=>{
+//     if (error) {
+//       console.error('El error de conexión es: ' + error);
+//       return;
+//     }
+//     console.log('¡Conectado a la Base de Datos!');
+//   });
+
+//   module.exports = connection;
+
+
+var pool  = mysql.createPool({
+    connectionLimit : 20,
+    host            : 'us-cdbr-east-05.cleardb.net',
+    user            : 'bf7ce2d52f141a',
+    password        : '4a1944fd',
+    database        : 'heroku_a1f7b4e8dda28ba'
+  });
+  
+  pool.query('SELECT * from users', function (error, results, fields) {
+    if (error) throw error;
+    //console.log(results[0]);
   });
 
-  module.exports = connection;
+  module.exports = pool;
+
+  
