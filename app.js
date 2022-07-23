6a93b5ea974ed1aeaf8f4f70b28c2420484e837d
rojasedwin <rojaedwin@gmail.com>
2022-07-23 17:09:18 -0400

    Cambios

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}))
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/router'))
 
