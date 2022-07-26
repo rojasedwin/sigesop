62a3b12d1ae834ea35234eee0f26f5763967bd0a
rojasedwin <rojaedwin@gmail.com>
2022-07-26 01:25:15 -0400

    Cambios

--- controllers/authController.js
+++ controllers/authController.js
@@ -123,9 +123,10 @@ exports.login = async (req, res)=>{
 
 //procedure to authenticate
 exports.isAuthenticated = async (req, res, next)=>{
-    if (req.cookies.jwt) {
+    if (req.cookies.jwt ) {
         try {
             
+           
             const decodificada = await promisify( jwt.verify )(req.cookies.jwt, process.env.JWT_SECRETO)
             conexion.query('SELECT * FROM users WHERE user_id = ?', [decodificada.id], (error, results)=>{
                 if(!results){return next()}
--- routes/router.js
+++ routes/router.js
@@ -8,30 +8,11 @@ const { Router } = require('express')
 const conexion = require('../database/db')
 
 
-
-/*conexion.getConnection(function(err, connection) {
-    // Usa la conexión
-    connection.query( 'SELECT * FROM users', function(err, rows) {
-        // Y listo con la conexión.
-        connection.release();
-        
-        // No use la conexión aquí, se ha devuelto al grupo.
-    });
-});*/
-
+//RUTAS GET
 router.get('/home',authController.isAuthenticated, (req, res) =>{
-    //res.render('index')
-    /*conexion.query('SELECT * from users', (error, results) =>{
-        if(error){
-            throw error
-        }else{
-            res.send(results)
-        }
-    })*/
-
+    
     conexion.getConnection(function(err, connection) {
-        // Usa la conexión
-       // connection.query('SELECT * FROM users WHERE user_email = ?', [email], function(error, rows) {
+
         connection.query( 'SELECT * FROM miembros where miembro_primera_vez=1', function(error, rows) {
             if(error){
                 throw error
@@ -43,35 +24,15 @@ router.get('/home',authController.isAuthenticated, (req, res) =>{
                     console.log(req.session.user_id)
                     //console.log(results)
                     res.render('home', {rows:rows, user_type:req.session.user_type, user_name:req.session.user_name, seguimiento:results})
-               
-               
-              
+
                 console.log('BD CONECTADA')
 
                 }) 
 
 
-               /* conexion.query('SELECT * FROM seguimientos as s INNER JOIN MIEMBROS as m ON m.miembro_id=s.seguimiento_id INNER JOIN users as u ON u.user_id=s.seguimiento_assigned_to WHERE s.seguimiento_assigned_to = ?', [req.session.user_id],function (error, rows)=>{
-
-                    if(error){
-                        throw error       
-                    }else{
-
-                    }
-
-                })  */
-
-
-                /*res.render('home', {rows:rows, user_type:req.session.user_type, user_name:req.session.user_name})
-               
-               
-               
-                console.log('BD CONECTADA')*/
                 
             }
-            // Y listo con la conexión.
             connection.release();
-            
             // No use la conexión aquí, se ha devuelto al grupo.
         });
     })
@@ -86,8 +47,31 @@ router.get('/', (req, res) => {
     res.render('index',{alert:false})
 })
 
+
+router.get('/usuarios',authController.isAuthenticated, (req, res) =>{
+    
+    conexion.getConnection(function(err, connection) {
+
+        connection.query( 'SELECT * FROM users', function(error, rows) {
+            if(error){
+                throw error
+            }else{
+
+                res.render('usuarios', {rows:rows, user_type:req.session.user_type, user_name:req.session.user_name})
+                 
+            }
+            connection.release();
+            // No use la conexión aquí, se ha devuelto al grupo.
+        });
+    })
+    
+})
+
 router.get('/logout', authController.logout)
 
+
+
+//RUTAS POST
 router.post('/registeruser', authController.registeruser)
 router.post('/login', authController.login)
 
